f835c067727ed05c7b7b7568515c54429c0ccb77
Mikhail <stv.kvz@gmail.com>
2018-01-22 18:30:03 +0300

    Signed-off-by: Mikhail <stv.kvz@gmail.com>

--- any2markdown.cmd
+++ any2markdown.cmd
@@ -3,14 +3,11 @@
 if "%~1"=="" goto continue
   set ext=%~x1
   set ext=%ext:~1%
+  set source=%1
   if %ext%==xhtml set ext=html
   if %ext%==htm set ext=html
-  pandoc --wrap=none --atx-headers -s -f %ext% -w markdown_mmd+yaml_metadata_block+pipe_tables-raw_html-mmd_header_identifiers -o "%~dpn1%~x1.md"  %1
-::  if errorlevel 0 echo %~nx1 =^> %~n1.mmd.md ok
-::  pandoc -s -f html -w markdown_strict-raw_html -o  "%~dpn1.strict.md"  %1
-::  if errorlevel 0 echo %~nx1 =^> %~n1.strict.md ok
-::  multimarkdown -t mmd -f --nolabels --nomask %1 -o "%~dpn1.mmd.mmark"
-::  if errorlevel 0 echo %~nx1 =^> %~n1.mmd.mmark ok
+  if %ext%==rtf (soffice --headless --convert-to odt %1)&&(set ext=odt)&&(set source="%~dpn1.odt")
+  pandoc --wrap=none --atx-headers -s -f %ext% -w markdown_mmd+yaml_metadata_block+pipe_tables-raw_html-mmd_header_identifiers -o "%~dpn1%~x1.md"  %source%
 shift
 goto loop
 :continue
---
+++ any2odt.cmd
@@ -0,0 +1,2 @@
+soffice --headless --convert-to odt %1
+pause
